73166c3e46cac11c4d25a9e30eeb37e7ba3b498e
wesinator <5124946+wesinator@users.noreply.github.com>
2019-12-31 10:04:39 -0500

    add Go implementation

---
+++ soaparse.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type SOArecord struct {
+	nameserver string
+
+	raw_email string
+	parsed_email string
+
+	date string
+
+	ttl string
+}
+
+// return index of second to last occurrence of char in string
+func SecondToLast(str string, char string) int {
+	return strings.LastIndex(str[:strings.LastIndex(str, char)], char)
+}
+
+// replace char given by index with replacement string
+func ReplaceCharAt(str, replacement string, index int) string {
+	return str[:index] + replacement + str[index + len(replacement):]
+}
+
+func EmailFromSOA(soaEmailField string) string {
+	var i int = SecondToLast(soaEmailField, ".")
+	var email string = ReplaceCharAt(soaEmailField, "@", i)
+	return email
+}
+
+func SOAparse (soaString string) SOArecord {
+	var parts []string = strings.Split(soaString, " ")
+
+	var soa SOArecord
+	soa.nameserver = strings.TrimSuffix(parts[0], ".")
+
+	soa.raw_email = strings.TrimSuffix(parts[1], ".")
+	soa.parsed_email = EmailFromSOA(soa.raw_email)
+
+	soa.date = parts[2]
+
+	soa.ttl = parts[3]
+
+	return soa
+}
+
+func main() {
+	var soa = SOAparse("ns.example.com. test.example.com. 20180203 60")
+	fmt.Println(soa)
+}
