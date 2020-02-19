598d143022fccb889d416a705fa01a8d401ed2f6
wesinator <5124946+wesinator@users.noreply.github.com>
2020-02-18 20:56:34 -0500

    add email from soa function to py module

--- python/soaparse/__init__.py
+++ python/soaparse/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .soaparse import SOArecord, soa_parse
+from .soaparse import SOArecord, soa_parse, email_from_soa
 
 __author__ = 'wesinator'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
